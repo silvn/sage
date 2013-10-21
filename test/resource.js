9f5c1a6d5c5473dbb5e695f7552baff7b6f346f6
Shiran Pasternak <shiranpasternak@gmail.com>
2013-10-21 12:44:25 -0400

    Get/Set of resource properties, with validation.

--- test/resource.js
+++ test/resource.js
@@ -4,4 +4,17 @@ describe("Resource", function () {
     it("should be a function", function () {
         Resource.should.be.a.Function;
     });
+    it("should define dynamic properties in constructor", function () {
+        var dog = new Resource({ type: { type: "any" } });
+        dog.type("Schnauzer").should.be.ok;
+        dog.type().should.equal("Schnauzer");
+    });
+    it("should throw an error on invalid data", function (done) {
+        var cat = new Resource({
+            food: { type: "string" }
+        });
+        (function () { cat.food(5) })
+            .should.throw("food must be of string type");
+        done();
+    });
 });
