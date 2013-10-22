a86410a851ff2ed8179b20f16414353a2b71359a
Shiran Pasternak <shiranpasternak@gmail.com>
2013-10-22 15:58:38 -0400

    Docs for the Sage container.

--- src/sage.js
+++ src/sage.js
@@ -2,8 +2,19 @@ var Service  = require("./service");
 var Resource = require("./resource");
 var Registry = require("./registry");
 
+/**
+ * @class Sage
+ * The toplevel class.
+ * 
+ * @singleton
+ */
 module.exports = {
+    /** @property {Service} Service The service class. */
     Service:  Service,
+
+    /** @property {Resource} Resource The resource class. */
     Resource: Resource,
+
+    /** @property {Registry} Registry The registry object. */
     Registry: Registry
 };
